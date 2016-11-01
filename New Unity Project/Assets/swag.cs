74b89fb0eba29c54c50568df76716296f1e0c930
Jelmer Le Mair <jelmerlm1@gmail.com>
2016-11-01 09:01:37 +0100

    main menu + gitignore
    main menu & de juiste git ignore. en 200+ niet ignorede files

---
+++ New Unity Project/Assets/swag.cs
@@ -0,0 +1,28 @@
+﻿    using UnityEngine;
+using System.Collections;
+public class PngReader : MonoBehaviour
+{
+    [SerializeField]
+    private Texture2D image;
+    [SerializeField]
+    private GameObject cube;
+    // Use this for initialization
+    void Start()
+    {
+        for (int x = 0; x < image.width; x++)
+        {
+            for (int y = 0; y < image.height; y++)
+            {
+                Color color = image.GetPixel(x, y);
+                float z = color.a + color.b + color.g + color.r;
+                GameObject c = Instantiate(cube, new Vector3(x, z, y), Quaternion.identity) as GameObject;
+                c.GetComponent<Renderer>().material.color = color;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
