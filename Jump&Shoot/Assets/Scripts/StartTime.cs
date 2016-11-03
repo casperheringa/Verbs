ee291a04a3581bd4eb7a8a8f87c22d48f414d0d2
ninoosewoudt <ninoosewoudt@Ninos-MacBook-Pro.local>
2016-11-03 09:27:20 +0100

    yoo

---
+++ Jump&Shoot/Assets/Scripts/StartTime.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartTime : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Time.timeScale = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.anyKey){
+			Time.timeScale = 1;
+		}
+	}
+}
